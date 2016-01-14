e2b038aa40f84fd5796de1ab2b46407ed9644e18
Taryn Parker <tarynp@Taryns-MacBook-Air.local>
2016-01-14 14:38:38 -0600

    added onbuttontapped

--- TicTacToe/ViewController.m
+++ TicTacToe/ViewController.m
@@ -29,9 +29,13 @@
     // Do any additional setup after loading the view, typically from a nib.
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (IBAction)onButtonTapped:(UIButton *)sender {
+    sender.titleLabel.text = self.whichPlayerLabel.text;
+    
+    
 }
+
+
+
 
 @end
