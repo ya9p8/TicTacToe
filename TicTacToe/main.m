7529fb924083ae2d1a73336d4c08e760e90d282b
Yemi Ajibola <ya9p8@mst.edu>
2016-01-14 13:33:10 -0600

    Initial Commit

---
+++ TicTacToe/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TicTacToe
+//
+//  Created by Yemi Ajibola on 1/14/16.
+//  Copyright © 2016 Yemi Ajibola. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
